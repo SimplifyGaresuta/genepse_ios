e5245c24f1e6405e8c985e5a61234c3e60dd2756
Kenta Iwami <g2117009@fun.ac.jp>
2017-12-07 05:23:26 +0900

    product以外のフォームを作成

--- GENEPSE/EditMyProfileViewController.swift
+++ GENEPSE/EditMyProfileViewController.swift
@@ -15,71 +15,117 @@ class EditMyProfileViewController: FormViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        self.view.backgroundColor = UIColor.blue
-        print(edit_id)
-        
-        //MARK: 公式にあったコード
-//        form +++ Section("Section1")
-//            <<< TextRow(){ row in
-//                row.title = "Text Row"
-//                row.placeholder = "Enter text here"
-//            }
-//            <<< PhoneRow(){
-//                $0.title = "Phone Row"
-//                $0.placeholder = "And numbers here"
-//            }
-//            +++ Section("Section2")
-//            <<< DateRow(){
-//                $0.title = "Date Row"
-//                $0.value = Date(timeIntervalSinceReferenceDate: 0)
-//        }
-        
-//        form +++
-//            MultivaluedSection(multivaluedOptions: [.Reorder, .Insert, .Delete],
-//                               header: "Multivalued TextField",
-//                               footer: ".Insert adds a 'Add Item' (Add New Tag) button row as last cell.") {
-//                                $0.addButtonProvider = { section in
-//                                    return ButtonRow(){
-//                                        $0.title = "Add New Tag"
-//                                    }
-//                                }
-//                                $0.multivaluedRowToInsertAt = { index in
-//                                    return NameRow() {
-//                                        $0.placeholder = "Tag Name"
-//                                    }
-//                                }
-//                                $0 <<< NameRow() {
-//                                    $0.placeholder = "Tag Name"
-//                                }
-//        }
-
-
+        CreateForms()
     }
     
-    //TODO: 各フォーム作成関数を呼び出す
+    //TODO: 各フォーム作成関数を呼び出す(必要ないかも?)
     func CreateForms() {
         switch edit_id {
         case SectionID.name.rawValue:
-            // textフィールド
-            // textview
+            form +++ Section("活動拠点")
+                <<< TextRow(){
+                    $0.title = ""
+                    $0.placeholder = "◯◯区"
+            }
+            
+            form +++ Section("自己紹介")
+                <<< TextAreaRow(){
+                    $0.title = ""
+                    $0.placeholder = "私は今までに独学でXXを勉強し…"
+            }
+            
             break
         case SectionID.awards.rawValue:
-            // textフィード(+)
+            form +++ MultivaluedSection(
+                multivaluedOptions: [.Reorder, .Insert, .Delete],
+               header: "受賞歴",
+               footer: "") {
+                $0.addButtonProvider = { section in return ButtonRow(){
+                    $0.title = "追加"
+                    }
+                }
+                $0.multivaluedRowToInsertAt = { index in return NameRow() {
+                    $0.placeholder = "◯◯賞(20XX)"
+                    }
+                }
+                $0 <<< TextRow() {
+                    $0.placeholder = "◯◯賞(20XX)"
+                }
+            }
+
             break
         case SectionID.skills.rawValue:
-            // textフィールド(+)
+            form +++ MultivaluedSection(
+                multivaluedOptions: [.Reorder, .Insert, .Delete],
+                header: "スキル",
+                footer: "プログラミング言語、使用可能ツールなど様々なスキルを1つずつ入力してください") {
+                    $0.addButtonProvider = { section in return ButtonRow(){
+                        $0.title = "追加"
+                        }
+                    }
+                    $0.multivaluedRowToInsertAt = { index in return TextRow() {
+                        $0.placeholder = "Python"
+                        }
+                    }
+                    $0 <<< TextRow() {
+                        $0.placeholder = "Python"
+                    }
+            }
             break
         case SectionID.products.rawValue:
             // textフィールド&&textフィールド,ImageRow(+)
             break
         case SectionID.sns.rawValue:
-            // picker&&textフィールド(+)
+            form +++ Section("Twitter")
+                <<< TextRow(){
+                    $0.title = ""
+                    $0.placeholder = "@◯◯◯◯◯◯"
+                    $0.value = "@◯◯◯◯◯◯"
+            }
             break
         case SectionID.license.rawValue:
-            // textフィールド(+)
+            form +++ MultivaluedSection(
+                multivaluedOptions: [.Reorder, .Insert, .Delete],
+                header: "資格",
+                footer: "") {
+                    $0.addButtonProvider = { section in return ButtonRow(){
+                        $0.title = "追加"
+                        }
+                    }
+                    $0.multivaluedRowToInsertAt = { index in return TextRow() {
+                        $0.placeholder = "◯◯管理技術者"
+                        }
+                    }
+                    $0 <<< TextRow() {
+                        $0.placeholder = "◯◯管理技術者"
+                    }
+                    $0 <<< TextRow() {
+                        $0.placeholder = "XX管理技術者"
+                    }
+            }
             break
         default:
-            // textフィールド*4(うち，number1つ)
+            form +++ Section("基本情報")
+                <<< SegmentedRow<String>("sex") {
+                    $0.options = ["男性", "女性", "その他"]
+                    $0.title = "性別"
+                    $0.value = "男性"
+                }
+            
+                <<< IntRow() {
+                    $0.title = "年齢"
+                    $0.placeholder = ""
+                }
+            
+                <<< TextRow(){
+                    $0.title = "居住地"
+                    $0.placeholder = "◯◯区"
+                }
+                
+                <<< TextRow(){
+                    $0.title = "最終学歴"
+                    $0.placeholder = "XX大学YY学部 卒業"
+                }
             break
         }
     }
