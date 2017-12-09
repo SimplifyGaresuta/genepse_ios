5adfc011d1642c51c72d27fd839213f7305b9d31
Kenta Iwami <g2117009@fun.ac.jp>
2017-12-09 15:47:03 +0900

    作品にバリでショーンのメッセージを設定

--- GENEPSE/ProductFromViewController.swift
+++ GENEPSE/ProductFromViewController.swift
@@ -29,6 +29,14 @@ class ProductFromViewController: FormViewController {
     }
     
     func CreateFrom() {
+        LabelRow.defaultCellUpdate = { cell, row in
+            cell.textLabel?.textColor = .white
+            cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 13)
+            cell.textLabel?.textAlignment = .right
+        }
+        let RuleRequired_M = "必須項目です"
+        let RuleRequired_Warning_M = "項目を埋めてアピール力をあげましょう"
+        
         let productImageView = AsyncUIImageView()
         
         form +++ Section("タイトル")
@@ -36,6 +44,24 @@ class ProductFromViewController: FormViewController {
                 $0.title = ""
                 $0.placeholder = "ポートフォリオサイト"
                 $0.value = product["title"].stringValue
+                $0.add(rule: RuleRequired())
+                $0.validationOptions = .validatesOnChange
+        }
+        .onRowValidationChanged { cell, row in
+            let rowIndex = row.indexPath!.row
+            while row.section!.count > rowIndex + 1 && row.section?[rowIndex  + 1] is LabelRow {
+                row.section?.remove(at: rowIndex + 1)
+            }
+            if !row.isValid {
+                for (index, _) in row.validationErrors.map({ $0.msg }).enumerated() {
+                    let labelRow = LabelRow() {
+                        $0.title = RuleRequired_M
+                        $0.cell.height = { 30 }
+                        $0.cell.backgroundColor = UIColor.red
+                    }
+                    row.section?.insert(labelRow, at: row.indexPath!.row + index + 1)
+                }
+            }
         }
         
         form +++ Section("URL")
@@ -43,21 +69,58 @@ class ProductFromViewController: FormViewController {
                 $0.title = ""
                 $0.placeholder = "http://◯◯.◯◯◯.◯◯"
                 $0.value = URL(string: product["url"].stringValue)
+                $0.add(rule: RuleRequired())
+                $0.validationOptions = .validatesOnChange
+        }
+        .onRowValidationChanged { cell, row in
+            let rowIndex = row.indexPath!.row
+            while row.section!.count > rowIndex + 1 && row.section?[rowIndex  + 1] is LabelRow {
+                row.section?.remove(at: rowIndex + 1)
+            }
+            if !row.isValid {
+                for (index, _) in row.validationErrors.map({ $0.msg }).enumerated() {
+                    let labelRow = LabelRow() {
+                        $0.title = RuleRequired_Warning_M
+                        $0.cell.height = { 30 }
+                        $0.cell.backgroundColor = UIColor.orange
+                    }
+                    row.section?.insert(labelRow, at: row.indexPath!.row + index + 1)
+                }
+            }
         }
         
         form +++ Section("画像")
             <<< ImageRow() {
-                
                 $0.title = "画像を選択する"
                 $0.sourceTypes = .PhotoLibrary
-                $0.clearAction = .no
+                $0.clearAction = .yes(style: UIAlertActionStyle.destructive)
+                $0.add(rule: RuleRequired())
+                $0.validationOptions = .validatesOnChange
+            }
+            .cellUpdate { cell, row in
+                cell.accessoryView?.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+                
+                if let image = row.value {
+                    productImageView.image = image
+                }else {
+                    productImageView.image = nil
                 }
-                .cellUpdate { cell, row in
-                    cell.accessoryView?.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
-                    
-                    if let image = row.value {
-                        productImageView.image = image
+        }
+        .onRowValidationChanged { cell, row in
+            let rowIndex = row.indexPath!.row
+            while row.section!.count > rowIndex + 1 && row.section?[rowIndex  + 1] is LabelRow {
+                row.section?.remove(at: rowIndex + 1)
+            }
+            if !row.isValid {
+                for (index, _) in row.validationErrors.map({ $0.msg }).enumerated() {
+                    let labelRow = LabelRow() {
+                        $0.title = RuleRequired_Warning_M
+                        $0.cell.height = { 30 }
+                        $0.cell.backgroundColor = UIColor.orange
                     }
+                    row.section?.insert(labelRow, at: row.indexPath!.row + index + 1)
+                }
+            }
         }
         
         let base_margin = self.view.frame.width * 0.1
