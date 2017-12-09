b89b28066d6559a9da37f39e4b11ad9ab380b544
Kenta Iwami <g2117009@fun.ac.jp>
2017-12-09 16:51:42 +0900

    enumにkeyを登録

--- GENEPSE/Setting.swift
+++ GENEPSE/Setting.swift
@@ -17,3 +17,33 @@ enum SectionID: Int {
     case license = 6
     case info = 7
 }
+
+enum Key: String {
+    // ユーザデータ
+    case activity_base = "activity_base"
+    case name = "name"
+    case overview = "overview"
+    case profile_img = "profile_img"
+    case attr = "attr"
+    case main_skills = "main_skills"
+    case awards = "awards"
+    case skills = "skills"
+    case products = "products"
+    case sns = "sns"
+    case license = "license"
+    case gender = "gender"
+    case age = "age"
+    case address = "address"
+    case school_career = "school_career"
+    
+    //プロダクト登録
+    case product_id = "product_id"
+    
+    // 位置情報
+    case latitude = "latitude"
+    case longitude = "longitude"
+    
+    // ログイン
+    case login_url = "login_url"
+    case user_id = "user_id"
+}
