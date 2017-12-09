c33b41850a3c7b76f3f5891ae07abfe82f013448
Kenta Iwami <g2117009@fun.ac.jp>
2017-12-09 21:28:55 +0900

    delete commentout

--- GENEPSE/ProductFromViewController.swift
+++ GENEPSE/ProductFromViewController.swift
@@ -154,15 +154,4 @@ class ProductFromViewController: FormViewController {
     func SetProduct(p: JSON) {
         product = p
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
