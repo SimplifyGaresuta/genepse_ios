9aea1656bbc2de9586ff8058ac956c85f80ad2f3
Kenta Iwami <g2117009@fun.ac.jp>
2017-12-10 16:08:33 +0900

    テストのため、ユーザデータ件数なしでFeedへ遷移するようにした

--- GENEPSE/AppDelegate.swift
+++ GENEPSE/AppDelegate.swift
@@ -16,7 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        if DBMethod().RecordCount(User.self) == 0 {
+        // TODO: テストのためデータなしでFeedへ遷移
+        if DBMethod().RecordCount(User.self) != 0 {
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
             let signupVC = storyboard.instantiateViewController(withIdentifier: "SignUp")
 
